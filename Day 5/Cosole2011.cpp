ea4615980ea62a503a832b8195e78767d3f3f4a5
tuan <tuan>
2020-11-21 08:27:26 +0700

    Đây là commit ngày 20/11/2020

---
+++ Day 5/Cosole2011.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    //Bài 43
+    // long i, n;
+    // int S;
+
+    // while(n <= 0 )
+	// {
+    //     printf("\n Nhap n: ");
+	// scanf("%2d", &n);
+    //     if(n <= 0){
+    //         printf("\n nhap n >0 !");
+    //     }
+	// }
+	// S = 0;
+	// i = n;
+
+	// while(i != 0)
+	// {
+	// 	S = S + 1;
+	// 	i = i / 10;
+	// }
+	// printf("\nSo chu so cua %ld la %d", n, S);
+    
+    //Bài 45
+    // long i, n;
+    // int S;
+
+    // while(n <= 0 )
+	// {
+    //     printf("\n Nhap n: ");
+	// scanf("%2d", &n);
+    //     if(n <= 0){
+    //         printf("\n nhap n >0 !");
+    //     }
+	// }
+	// S = 0;
+	// i = n;
+
+	// while(i != 0)
+	// {
+	// 	S = S + i % 10;
+	// 	i = i / 10;
+	// }
+	// printf("\nTong la %d", S);
+
+    //Bài 46
+
+}
